eecea6692ccb298fa5a4600fa41f2e86fd23c105
yopa <90709631+yopaaa@users.noreply.github.com>
2023-07-13 08:49:28 +0700

    Add Bottom Sheet

--- App.js
+++ App.js
@@ -1,30 +1,73 @@
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import Navigation from './pages/components/Navigation'
+
+// import 'react-native-gesture-handler'
+import { StatusBar } from 'expo-status-bar'
+import { BottomSheetModal, BottomSheetModalProvider, BottomSheetScrollView } from '@gorhom/bottom-sheet'
+import { useEffect, useRef } from 'react'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
 import Setting from './pages/Setting'
 import Color from './pages/Color'
 import Customize from './pages/Custom'
 
 const Stack = createNativeStackNavigator()
 
-export default function App() {
-  const pageList = ['Color', 'Customize', 'Setting']
+function Home() {
+  const bottonSheetModalRef = useRef(null)
 
+  const snapPoints = ['10%', '80%']
+
+  function showSheet() {
+    bottonSheetModalRef.current?.present()
+  }
+  useEffect(() => {
+    showSheet()
+  }, [])
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <BottomSheetModalProvider>
+        <View style={styles.container}>
+          <Color />
+          <StatusBar style="auto" />
+          <BottomSheetModal
+            onDismiss={(x) => showSheet()}
+            ref={bottonSheetModalRef}
+            index={0}
+            snapPoints={snapPoints}
+            backgroundStyle={{ borderRadius: 30, borderWidth: 1, borderColor: 'black' }}
+          >
+            <BottomSheetScrollView>
+              <View>
+                <Customize />
+              </View>
+            </BottomSheetScrollView>
+          </BottomSheetModal>
+        </View>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
+  )
+}
+
+export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Color" component={Color} options={{ headerShown: false }} />
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
 
         <Stack.Screen name="Customize" component={Customize} options={{ headerShown: false }} />
 
         <Stack.Screen name="Setting" component={Setting} options={{ headerShown: false }} />
-
       </Stack.Navigator>
-
-      <View style={{ backgroundColor: '#353542' }}>
-        <Navigation pageList={pageList} defaultPage={'Color'} />
-      </View>
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
