ff7963011fe258714bf3a1d171dd4536f8d10c38
yopa <90709631+yopaaa@users.noreply.github.com>
2023-07-18 09:39:11 +0700

    Split code

--- App.js
+++ App.js
@@ -1,74 +1,35 @@
-import { View, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-// import 'react-native-gesture-handler'
-import { StatusBar } from 'expo-status-bar'
-import { BottomSheetModal, BottomSheetModalProvider, BottomSheetScrollView } from '@gorhom/bottom-sheet'
-import { useEffect, useRef } from 'react'
-import { GestureHandlerRootView } from 'react-native-gesture-handler'
-
 import Setting from './pages/Setting'
-import Color from './pages/Color'
-import Customize from './pages/Custom'
+import BottomSheet from './pages/BottomSheet'
+import ScanQR from './pages/ScanQR'
 
 const Stack = createNativeStackNavigator()
-
-function Home() {
-  const bottonSheetModalRef = useRef(null)
-
-  const snapPoints = ['15%', '80%']
-
-  function showSheet() {
-    bottonSheetModalRef.current?.present()
-  }
-  useEffect(() => {
-    showSheet()
-  }, [])
-  return (
-    <GestureHandlerRootView style={{ flex: 1 }}>
-      <BottomSheetModalProvider>
-        <View style={styles.container}>
-          <Color />
-          <StatusBar style="auto" />
-          <BottomSheetModal
-            onDismiss={(x) => showSheet()}
-            ref={bottonSheetModalRef}
-            index={0}
-            snapPoints={snapPoints}
-            backgroundStyle={{ borderRadius: 30, backgroundColor: '#333333' }}
-          >
-            <BottomSheetScrollView>
-              <View>
-                <Customize />
-              </View>
-            </BottomSheetScrollView>
-          </BottomSheetModal>
-        </View>
-      </BottomSheetModalProvider>
-    </GestureHandlerRootView>
-  )
-}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
-      >
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={BottomSheet} options={{ headerShown: false }} />
 
-        <Stack.Screen name="Customize" component={Customize} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Setting"
+          component={Setting}
+          options={{
+            headerShown: false,
+            animation: 'slide_from_right'
+          }}
+        />
 
-        <Stack.Screen name="Setting" component={Setting} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="ScanQR"
+          component={ScanQR}
+          options={{
+            headerShown: false,
+            animation: 'slide_from_left'
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
